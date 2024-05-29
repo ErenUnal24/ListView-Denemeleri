bfaf99c1bf3f2e7dc130b04dfd0e4d3b1de2378f
ErenUnal24 <bilalunal24@gmail.com>
2024-05-29 16:04:51 +0300

    Initial Commit

---
+++ ListViewDenemesi/ListViewDenemesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewDenemesiApp.swift
+//  ListViewDenemesi
+//
+//  Created by Eren on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ListViewDenemesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
